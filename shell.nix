749753316886cfa7a45a12aee0e36a7ef056148d
Atharva Amritkar <atharvaamritkar@protonmail.com>
2022-04-25 00:12:17 +0530

    restructuring integrationtest.sh and shell.nix
    Removed SRC_DIR and BIN_DIR from integrationtest script as we will be using binaries generated by nix expressions

--- shell.nix
+++ shell.nix
@@ -1,15 +1,4 @@
-{ pkgs ? import <nixpkgs> {
-    overlays = [
-(import "${(fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz")}/overlay.nix")
-      (self: super: {
-          rustc = super.fenix.latest.rustc;
-          cargo  = super.fenix.latest.cargo;
-          rust-src = super.fenix.latest.rust-src;
-      }
-        )
-    ];
-  }
-}:
+{ pkgs ? import <nixpkgs> {}}:
 
 pkgs.mkShell {
   packages = with pkgs; [
@@ -20,7 +9,9 @@ pkgs.mkShell {
     clightning
     jq
   ];
+buildInputs = [
+   (import ./default.nix )
+   ];
 
-  RUST_SRC_PATH = "${pkgs.rust-src}/lib/rustlib/src/rust/library";
 }
 
