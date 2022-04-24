cbaffb37d8a1e32d8e59c91ca08a0c3be5d68ee0
Atharva Amritkar <atharvaamritkar@protonmail.com>
2022-04-24 23:23:20 +0530

    Masking fenix overlay and adding tools as needed

--- shell.nix
+++ shell.nix
@@ -1,15 +1,4 @@
 { pkgs ? import <nixpkgs> {
-    overlays = [
-(import "${(fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz")}/overlay.nix")
-      (self: super: {
-          rustc = super.fenix.latest.rustc;
-          cargo  = super.fenix.latest.cargo;
-          rust-src = super.fenix.latest.rust-src;
-      }
-        )
-    ];
-  }
-}:
 
 pkgs.mkShell {
   packages = with pkgs; [
@@ -20,9 +9,10 @@ pkgs.mkShell {
     clightning
     jq
   ];
+
 buildInputs = [
-  (import ./default.nix )
-];
-  RUST_SRC_PATH = "${pkgs.rust-src}/lib/rustlib/src/rust/library";
+   (import ./default.nix )
+   ];
+
 }
 
