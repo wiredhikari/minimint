a31d18ddde496131a74c63d1719e4b18a0a63d61
Atharva <atharvaamritkar@protonmail.com>
2022-06-16 01:03:12 +0530

    crane

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default = crane.lib.${system}.buildPackage {
+        src = ./.;
+
+        # Add extra inputs here or any other derivation settings
+        # doCheck = true;
+        # buildInputs = [];
+        # nativeBuildInputs = [];
+      };
+    });
+}
