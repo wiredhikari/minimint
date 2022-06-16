2ff57586e82a235d7d7ca1e7cacff692270f9849
Atharva <atharvaamritkar@protonmail.com>
2022-06-16 10:19:35 +0530

    refac crane

--- flake.nix
+++ flake.nix
@@ -15,7 +15,6 @@
 
         craneLib = crane.lib.${system};
 
-        # Common derivation arguments used for all builds
         commonArgs = {
           src = ./.;
 
@@ -26,41 +25,23 @@
           ];
 
           nativeBuildInputs = with pkgs; [
-            # Add extra native build inputs here, etc.
             pkg-config
           ];
         };
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
         cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {
-          # Additional arguments specific to this derivation can be added here.
-          # Be warned that using `//` will not do a deep copy of nested
-          # structures
           pname = "mycrate-deps";
         });
 
-        # Run clippy (and deny all warnings) on the crate source,
-        # resuing the dependency artifacts (e.g. from build scripts or
-        # proc-macros) from above.
-        #
-        # Note that this is done as a separate derivation so it
-        # does not impact building just the crate by itself.
         myCrateClippy = craneLib.cargoClippy (commonArgs // {
-          # Again we apply some extra arguments only to this derivation
-          # and not every where else. In this case we add some clippy flags
           inherit cargoArtifacts;
           cargoClippyExtraArgs = "-- --deny warnings";
         });
 
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
         myCrate = craneLib.buildPackage (commonArgs // {
           inherit cargoArtifacts;
         });
 
-        # Also run the crate tests under cargo-tarpaulin so that we can keep
-        # track of code coverage
         myCrateCoverage = craneLib.cargoTarpaulin (commonArgs // {
           inherit cargoArtifacts;
         });
@@ -69,7 +50,6 @@
         packages.default = myCrate;
         checks = {
          inherit
-           # Build the crate as part of `nix flake check` for convenience
            myCrate
            myCrateClippy
            myCrateCoverage;
